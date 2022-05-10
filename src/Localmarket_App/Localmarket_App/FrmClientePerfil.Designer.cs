d8dce84b79f0fec4dc8dfaffe98950447e952db0
RobertoMartinez <romaav@floridauniversitaria.es>
2022-05-10 20:01:59 +0200

    Creacion de formulario de perfil de usuario cliente

--- src/Localmarket_App/Localmarket_App/FrmClientePerfil.Designer.cs
+++ src/Localmarket_App/Localmarket_App/FrmClientePerfil.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Localmarket_App
 {
-    partial class frmEmpresaPerfil
+    partial class frmClientePerfil
     {
         /// <summary>
         /// Required designer variable.
@@ -29,30 +29,30 @@ namespace Localmarket_App
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmEmpresaPerfil));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmClientePerfil));
             this.pnlTaronja = new System.Windows.Forms.Panel();
-            this.lblNombre = new System.Windows.Forms.Label();
-            this.lblAcceso = new System.Windows.Forms.Label();
-            this.lblCrear = new System.Windows.Forms.Label();
-            this.lblAjustes2 = new System.Windows.Forms.Label();
             this.lblNotificaciones = new System.Windows.Forms.Label();
+            this.lblAjustes2 = new System.Windows.Forms.Label();
+            this.lblCrear = new System.Windows.Forms.Label();
+            this.lblAcceso = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.picAjustes1 = new System.Windows.Forms.PictureBox();
+            this.picPerf = new System.Windows.Forms.PictureBox();
+            this.picLogo = new System.Windows.Forms.PictureBox();
             this.picNotificaciones = new System.Windows.Forms.PictureBox();
             this.picAjustes2 = new System.Windows.Forms.PictureBox();
             this.picCrear = new System.Windows.Forms.PictureBox();
             this.picAcceso = new System.Windows.Forms.PictureBox();
             this.picPfp = new System.Windows.Forms.PictureBox();
-            this.picAjustes1 = new System.Windows.Forms.PictureBox();
-            this.picPerf = new System.Windows.Forms.PictureBox();
-            this.picLogo = new System.Windows.Forms.PictureBox();
             this.pnlTaronja.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picAjustes1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPerf)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picNotificaciones)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picAjustes2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picCrear)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picAcceso)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picPfp)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.picAjustes1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.picPerf)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).BeginInit();
             this.SuspendLayout();
             // 
             // pnlTaronja
@@ -65,111 +65,57 @@ namespace Localmarket_App
             this.pnlTaronja.Location = new System.Drawing.Point(0, 0);
             this.pnlTaronja.Name = "pnlTaronja";
             this.pnlTaronja.Size = new System.Drawing.Size(1342, 100);
-            this.pnlTaronja.TabIndex = 1;
-            // 
-            // lblNombre
-            // 
-            this.lblNombre.AutoSize = true;
-            this.lblNombre.Font = new System.Drawing.Font("Leelawadee", 24F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNombre.Location = new System.Drawing.Point(414, 142);
-            this.lblNombre.Name = "lblNombre";
-            this.lblNombre.Size = new System.Drawing.Size(345, 48);
-            this.lblNombre.TabIndex = 3;
-            this.lblNombre.Text = "Nombre Comercio";
-            // 
-            // lblAcceso
-            // 
-            this.lblAcceso.AutoSize = true;
-            this.lblAcceso.Font = new System.Drawing.Font("Leelawadee", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblAcceso.Location = new System.Drawing.Point(491, 219);
-            this.lblAcceso.Name = "lblAcceso";
-            this.lblAcceso.Size = new System.Drawing.Size(196, 29);
-            this.lblAcceso.TabIndex = 8;
-            this.lblAcceso.Text = "Acceder a pagina";
-            // 
-            // lblCrear
-            // 
-            this.lblCrear.AutoSize = true;
-            this.lblCrear.Font = new System.Drawing.Font("Leelawadee", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCrear.Location = new System.Drawing.Point(491, 304);
-            this.lblCrear.Name = "lblCrear";
-            this.lblCrear.Size = new System.Drawing.Size(359, 29);
-            this.lblCrear.TabIndex = 9;
-            this.lblCrear.Text = "Crear nueva pagina de comercio";
-            // 
-            // lblAjustes2
-            // 
-            this.lblAjustes2.AutoSize = true;
-            this.lblAjustes2.Font = new System.Drawing.Font("Leelawadee", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblAjustes2.Location = new System.Drawing.Point(491, 388);
-            this.lblAjustes2.Name = "lblAjustes2";
-            this.lblAjustes2.Size = new System.Drawing.Size(402, 29);
-            this.lblAjustes2.TabIndex = 10;
-            this.lblAjustes2.Text = "Modificar ajustes de paginas creadas";
+            this.pnlTaronja.TabIndex = 12;
             // 
             // lblNotificaciones
             // 
             this.lblNotificaciones.AutoSize = true;
             this.lblNotificaciones.Font = new System.Drawing.Font("Leelawadee", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNotificaciones.Location = new System.Drawing.Point(491, 473);
+            this.lblNotificaciones.Location = new System.Drawing.Point(491, 506);
             this.lblNotificaciones.Name = "lblNotificaciones";
-            this.lblNotificaciones.Size = new System.Drawing.Size(366, 29);
-            this.lblNotificaciones.TabIndex = 11;
-            this.lblNotificaciones.Text = "Notificaciones de nuevas reviews";
+            this.lblNotificaciones.Size = new System.Drawing.Size(400, 29);
+            this.lblNotificaciones.TabIndex = 22;
+            this.lblNotificaciones.Text = "Notificaciones de comercios listados";
             // 
-            // picNotificaciones
+            // lblAjustes2
             // 
-            this.picNotificaciones.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.picNotificaciones.Image = global::Localmarket_App.Properties.Resources.bell;
-            this.picNotificaciones.Location = new System.Drawing.Point(422, 463);
-            this.picNotificaciones.Name = "picNotificaciones";
-            this.picNotificaciones.Size = new System.Drawing.Size(48, 48);
-            this.picNotificaciones.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.picNotificaciones.TabIndex = 7;
-            this.picNotificaciones.TabStop = false;
+            this.lblAjustes2.AutoSize = true;
+            this.lblAjustes2.Font = new System.Drawing.Font("Leelawadee", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAjustes2.Location = new System.Drawing.Point(491, 421);
+            this.lblAjustes2.Name = "lblAjustes2";
+            this.lblAjustes2.Size = new System.Drawing.Size(288, 29);
+            this.lblAjustes2.TabIndex = 21;
+            this.lblAjustes2.Text = "Modificar ajustes de perfil";
             // 
-            // picAjustes2
+            // lblCrear
             // 
-            this.picAjustes2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.picAjustes2.Image = global::Localmarket_App.Properties.Resources.gear;
-            this.picAjustes2.Location = new System.Drawing.Point(422, 378);
-            this.picAjustes2.Name = "picAjustes2";
-            this.picAjustes2.Size = new System.Drawing.Size(48, 48);
-            this.picAjustes2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.picAjustes2.TabIndex = 6;
-            this.picAjustes2.TabStop = false;
+            this.lblCrear.AutoSize = true;
+            this.lblCrear.Font = new System.Drawing.Font("Leelawadee", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCrear.Location = new System.Drawing.Point(491, 337);
+            this.lblCrear.Name = "lblCrear";
+            this.lblCrear.Size = new System.Drawing.Size(339, 29);
+            this.lblCrear.TabIndex = 20;
+            this.lblCrear.Text = "Crear nueva lista de comercios";
             // 
-            // picCrear
+            // lblAcceso
             // 
-            this.picCrear.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.picCrear.Image = ((System.Drawing.Image)(resources.GetObject("picCrear.Image")));
-            this.picCrear.Location = new System.Drawing.Point(422, 294);
-            this.picCrear.Name = "picCrear";
-            this.picCrear.Size = new System.Drawing.Size(48, 48);
-            this.picCrear.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.picCrear.TabIndex = 5;
-            this.picCrear.TabStop = false;
+            this.lblAcceso.AutoSize = true;
+            this.lblAcceso.Font = new System.Drawing.Font("Leelawadee", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAcceso.Location = new System.Drawing.Point(491, 252);
+            this.lblAcceso.Name = "lblAcceso";
+            this.lblAcceso.Size = new System.Drawing.Size(315, 29);
+            this.lblAcceso.TabIndex = 19;
+            this.lblAcceso.Text = "Acceder a lista de comercios";
             // 
-            // picAcceso
+            // lblNombre
             // 
-            this.picAcceso.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.picAcceso.Image = ((System.Drawing.Image)(resources.GetObject("picAcceso.Image")));
-            this.picAcceso.Location = new System.Drawing.Point(422, 209);
-            this.picAcceso.Name = "picAcceso";
-            this.picAcceso.Size = new System.Drawing.Size(48, 48);
-            this.picAcceso.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.picAcceso.TabIndex = 4;
-            this.picAcceso.TabStop = false;
-            // 
-            // picPfp
-            // 
-            this.picPfp.Image = global::Localmarket_App.Properties.Resources.perfil;
-            this.picPfp.Location = new System.Drawing.Point(231, 209);
-            this.picPfp.Name = "picPfp";
-            this.picPfp.Size = new System.Drawing.Size(153, 150);
-            this.picPfp.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.picPfp.TabIndex = 2;
-            this.picPfp.TabStop = false;
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Leelawadee", 24F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(414, 175);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(311, 48);
+            this.lblNombre.TabIndex = 14;
+            this.lblNombre.Text = "Nombre Usuario";
             // 
             // picAjustes1
             // 
@@ -202,12 +148,67 @@ namespace Localmarket_App
             this.picLogo.TabIndex = 0;
             this.picLogo.TabStop = false;
             // 
-            // frmEmpresaPerfil
+            // picNotificaciones
+            // 
+            this.picNotificaciones.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.picNotificaciones.Image = global::Localmarket_App.Properties.Resources.bell;
+            this.picNotificaciones.Location = new System.Drawing.Point(422, 496);
+            this.picNotificaciones.Name = "picNotificaciones";
+            this.picNotificaciones.Size = new System.Drawing.Size(48, 48);
+            this.picNotificaciones.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picNotificaciones.TabIndex = 18;
+            this.picNotificaciones.TabStop = false;
+            // 
+            // picAjustes2
+            // 
+            this.picAjustes2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.picAjustes2.Image = global::Localmarket_App.Properties.Resources.gear;
+            this.picAjustes2.Location = new System.Drawing.Point(422, 411);
+            this.picAjustes2.Name = "picAjustes2";
+            this.picAjustes2.Size = new System.Drawing.Size(48, 48);
+            this.picAjustes2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picAjustes2.TabIndex = 17;
+            this.picAjustes2.TabStop = false;
+            // 
+            // picCrear
+            // 
+            this.picCrear.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.picCrear.Image = ((System.Drawing.Image)(resources.GetObject("picCrear.Image")));
+            this.picCrear.Location = new System.Drawing.Point(422, 327);
+            this.picCrear.Name = "picCrear";
+            this.picCrear.Size = new System.Drawing.Size(48, 48);
+            this.picCrear.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picCrear.TabIndex = 16;
+            this.picCrear.TabStop = false;
+            // 
+            // picAcceso
+            // 
+            this.picAcceso.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.picAcceso.Image = ((System.Drawing.Image)(resources.GetObject("picAcceso.Image")));
+            this.picAcceso.Location = new System.Drawing.Point(422, 242);
+            this.picAcceso.Name = "picAcceso";
+            this.picAcceso.Size = new System.Drawing.Size(48, 48);
+            this.picAcceso.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picAcceso.TabIndex = 15;
+            this.picAcceso.TabStop = false;
+            // 
+            // picPfp
+            // 
+            this.picPfp.Image = global::Localmarket_App.Properties.Resources.perfil;
+            this.picPfp.Location = new System.Drawing.Point(231, 242);
+            this.picPfp.Name = "picPfp";
+            this.picPfp.Size = new System.Drawing.Size(153, 150);
+            this.picPfp.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picPfp.TabIndex = 13;
+            this.picPfp.TabStop = false;
+            // 
+            // frmClientePerfil
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.AntiqueWhite;
             this.ClientSize = new System.Drawing.Size(1342, 703);
+            this.Controls.Add(this.pnlTaronja);
             this.Controls.Add(this.lblNotificaciones);
             this.Controls.Add(this.lblAjustes2);
             this.Controls.Add(this.lblCrear);
@@ -218,18 +219,17 @@ namespace Localmarket_App
             this.Controls.Add(this.picAcceso);
             this.Controls.Add(this.lblNombre);
             this.Controls.Add(this.picPfp);
-            this.Controls.Add(this.pnlTaronja);
-            this.Name = "frmEmpresaPerfil";
-            this.Text = "º";
+            this.Name = "frmClientePerfil";
+            this.Text = "Perfil de Cliente";
             this.pnlTaronja.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picAjustes1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPerf)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picNotificaciones)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picAjustes2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picCrear)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picAcceso)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picPfp)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.picAjustes1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.picPerf)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -237,19 +237,19 @@ namespace Localmarket_App
 
         #endregion
 
+        private System.Windows.Forms.PictureBox picLogo;
         private System.Windows.Forms.Panel pnlTaronja;
         private System.Windows.Forms.PictureBox picAjustes1;
         private System.Windows.Forms.PictureBox picPerf;
-        private System.Windows.Forms.PictureBox picLogo;
-        private System.Windows.Forms.PictureBox picPfp;
-        private System.Windows.Forms.Label lblNombre;
-        private System.Windows.Forms.PictureBox picAcceso;
-        private System.Windows.Forms.PictureBox picCrear;
+        private System.Windows.Forms.Label lblNotificaciones;
+        private System.Windows.Forms.Label lblAjustes2;
+        private System.Windows.Forms.Label lblCrear;
+        private System.Windows.Forms.Label lblAcceso;
         private System.Windows.Forms.PictureBox picNotificaciones;
         private System.Windows.Forms.PictureBox picAjustes2;
-        private System.Windows.Forms.Label lblAcceso;
-        private System.Windows.Forms.Label lblCrear;
-        private System.Windows.Forms.Label lblAjustes2;
-        private System.Windows.Forms.Label lblNotificaciones;
+        private System.Windows.Forms.PictureBox picCrear;
+        private System.Windows.Forms.PictureBox picAcceso;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.PictureBox picPfp;
     }
 }
