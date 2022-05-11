f370fd33c2b60b561a6d904b260624fb594e11dd
ALUMNADO\vicadi <vicadi@floridauniversitaria.es>
2022-05-11 18:22:37 +0200

    Avanzado codigo sql bbdd: creación inicial de la base de datos.

---
+++ bbdd/Codigo SQL/LocalMarket_DB.sql
@@ -0,0 +1,11 @@
+## BASE DE DATOS Local Market App 
+## Inicio código de creación de base de datos 
+DROP DATABASE localmarket_db IF EXISTS;
+
+CREATE DATABASE IF NOT EXISTS  localmarket_db CHARACTER SET UTF8;
+
+USE localmarket_db;
+
+## Procedemos a la creación de las tablas de la base de datos:
+## Tabla Usuarios:
+##CREATE TABLE IF NOT EXISTS 
