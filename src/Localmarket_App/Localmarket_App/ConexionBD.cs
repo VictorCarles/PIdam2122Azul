f4a0d583ad8b538719cb1f27599bce4413a9f165
ALUMNADO\vigigi01 <vigigi01@floridauniversitaria.com>
2022-05-16 16:22:35 +0200

    Creada clase de ConexionBD y agrgadas referencias

---
+++ src/Localmarket_App/Localmarket_App/ConexionBD.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+
+namespace Aev7
+{
+    class ConexionBD
+    {
+        private static MySqlConnection instancia = null;
+
+        private static readonly object padlock = new object();
+
+        private ConexionBD() { }
+
+        public static MySqlConnection Conexion
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instancia == null)
+                    {
+                        instancia = new MySqlConnection("Server=localhost;Database=aev7;Uid=root;Pwd=;");
+                    }
+                    return instancia;
+                }
+            }
+        }
+        /// <summary>
+        /// Abre la conexion a la base de datos
+        /// </summary>
+        public static void AbrirConexion()
+        {
+            if (instancia != null)
+                instancia.Open();
+        }
+        /// <summary>
+        /// Cierra la conexion a la base de datos
+        /// </summary>
+        public static void CerrarConexion()
+        {
+            if (instancia != null)
+                instancia.Close();
+        }
+
+    }
+}
