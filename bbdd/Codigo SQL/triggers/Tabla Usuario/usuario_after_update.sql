58925a75ada5e3bc80a193d8d538276f81d2ebdc
VictorCarles <vicadi@floridauniversitaria.es>
2022-05-21 16:43:12 +0200

    Corregido trigger usuario_after_update. Ampliada funcionalidad.

--- bbdd/Codigo SQL/triggers/Tabla Usuario/usuario_after_update.sql
+++ bbdd/Codigo SQL/triggers/Tabla Usuario/usuario_after_update.sql
@@ -1,5 +1,23 @@
-CREATE TRIGGER `usuario_before_update` BEFORE UPDATE ON `usuario` FOR EACH ROW BEGIN
+CREATE TRIGGER `usuario_after_update` AFTER UPDATE ON `usuario` FOR EACH ROW BEGIN
 
-INSERT INTO Acciones VALUES (DEFAULT, username+' a actualizado su perfil.', NOW(), username, NULL);
+DECLARE varcif VARCHAR(20);
+DECLARE acc VARCHAR(600);
+
+SET acc='Accion';
+SET varcif=NULL;
+
+SET varcif=(SELECT cif FROM localmarket_db.empresa WHERE owner_username=username);
+
+if (NEW.tipo!=OLD.tipo && NEW.tipo='business') THEN
+		SET acc=username+' ha actualizado su cuenta al tipo EMPRESARIO.';
+		SET varcif=(SELECT cif FROM localmarket_db.empresa WHERE owner_username=username);
+END if;
+if (NEW.tipo!=OLD.tipo && NEW.tipo='basic') THEN
+		SET acc=username+' ha actualizado su cuenta al tipo BASIC.';
+		SET varcif=NULL;
+END IF;		
+
+INSERT INTO Acciones (action_id, accion, fecha, user_username, cif_company)
+VALUES (DEFAULT, acc, NOW(), username, varcif);
 
 END
