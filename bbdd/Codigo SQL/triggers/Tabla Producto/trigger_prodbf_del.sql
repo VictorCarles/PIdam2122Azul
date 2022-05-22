a08c88cef1307e1212b88ea7eb0a0e443b334922
VictorCarles <vicadi@floridauniversitaria.es>
2022-05-22 13:07:14 +0200

    Corregido trigger_prodbf_del.sql. Guardada nueva versión sin errores en " producto_before_delete.sql "

--- bbdd/Codigo SQL/triggers/Tabla Producto/trigger_prodbf_del.sql
+++ bbdd/Codigo SQL/triggers/Tabla Producto/trigger_prodbf_del.sql
@@ -1,3 +1,3 @@
 BEGIN
-INSERT INTO Acciones VALUE (DEFAULT,prod_name+' eliminado de catalogo.',CURDATE(),NULL,cifEmp);
+INSERT INTO Acciones VALUE (DEFAULT, prod_name+' eliminado de catalogo.',CURDATE(),NULL,cifEmp);
 END
